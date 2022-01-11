a5836848fc464518b3a7e29839cd6f5b0f0837aa
rhdtn311 <68289543+rhdtn311@users.noreply.github.com>
2022-01-12 00:14:09 +0900

    21-01-11) querydsl JPAQueryFactory 빈 등록

--- src/main/java/kong/tues/commons/config/WebConfig.java
+++ src/main/java/kong/tues/commons/config/WebConfig.java
@@ -1,9 +1,14 @@
 package kong.tues.commons.config;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import kong.tues.commons.interceptor.LoginInterceptor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
@@ -14,5 +19,13 @@ public class WebConfig implements WebMvcConfigurer {
                 .order(1)
                 .addPathPatterns("/**")
                 .excludePathPatterns("/member/**", "/find/**", "/error");
+    }
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Bean
+    public JPAQueryFactory jpaQueryFactory() {
+        return new JPAQueryFactory(entityManager);
     }
 }
