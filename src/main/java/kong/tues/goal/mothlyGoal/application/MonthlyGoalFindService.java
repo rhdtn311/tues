593d00ae6209e2b9b285f1cde8d550730f8e28b8
rhdtn311 <68289543+rhdtn311@users.noreply.github.com>
2022-01-21 13:04:12 +0900

    22-01-21 : 사용하지 않는 코드 제거

--- src/main/java/kong/tues/goal/mothlyGoal/application/MonthlyGoalFindService.java
+++ src/main/java/kong/tues/goal/mothlyGoal/application/MonthlyGoalFindService.java
@@ -13,8 +13,6 @@ import java.util.List;
 @Service
 public class MonthlyGoalFindService {
 
-    private final MonthlyGoalRepository monthlyGoalRepository;
-
     private final MonthlyGoalQueryRepository monthlyGoalQueryRepository;
 
     @Transactional
