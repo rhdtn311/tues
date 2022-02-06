6debf6fd98e42663281c0122aafa95fcd7f2f563
rhdtn311 <68289543+rhdtn311@users.noreply.github.com>
2022-02-07 01:59:34 +0900

    22-02-07) 로그추가

--- src/main/java/kong/tues/member/exception/MemberExControllerAdvice.java
+++ src/main/java/kong/tues/member/exception/MemberExControllerAdvice.java
@@ -21,7 +21,7 @@ public class MemberExControllerAdvice {
 
         model.addAttribute("errorMember", errorMember);
 
-        return "/member/join";
+        return "/join";
     }
 
     @ExceptionHandler(MailExistException.class)
@@ -35,20 +35,20 @@ public class MemberExControllerAdvice {
 
         model.addAttribute("errorMember", errorMember);
 
-        return "/member/join";
+        return "/join";
     }
 
     @ExceptionHandler(LoginFailException.class)
     public String loginFail(LoginFailException error, Model model) {
         model.addAttribute("error", error.getMessage());
 
-        return "/member/login";
+        return "/login";
     }
 
     @ExceptionHandler(MemberNotFoundException.class)
     public String memberNotFound(MemberNotFoundException error, Model model) {
         model.addAttribute("error", error.getMessage());
 
-        return "/member/find";
+        return "/find";
     }
 }
