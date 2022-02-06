4c998d5918f14d5f17b6f26300e4d987646ed73f
rhdtn311 <68289543+rhdtn311@users.noreply.github.com>
2022-02-07 01:27:39 +0900

    22-02-07) 로그추가

--- src/main/java/kong/tues/member/application/JoinService.java
+++ src/main/java/kong/tues/member/application/JoinService.java
@@ -21,16 +21,23 @@ public class JoinService {
 
     public void join(MemberJoinReqDto memberJoinReqDto) {
 
+        log.info("joinService - 1");
         Member member = memberJoinReqDto.toMember();
         if (memberRepository.existsMemberByLoginId(member.getLoginId())) {
+            log.info("joinService - 2");
             throw new LoginIdExistException(memberJoinReqDto);
         }
 
+        log.info("joinService - 3");
         if (memberRepository.existsMemberByMail(member.getMail())) {
+            log.info("joinService - 4");
             throw new MailExistException(memberJoinReqDto);
         }
 
+        log.info("joinService - 5");
         member.passwordEncode(passwordEncoder);
+        log.info("joinService - 6");
         memberRepository.save(member);
+        log.info("joinService - 7");
     }
 }
