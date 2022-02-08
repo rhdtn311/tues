9f12ca8575d0314829a37e86fdfd3358a9a932f2
rhdtn311 <68289543+rhdtn311@users.noreply.github.com>
2022-02-08 12:08:59 +0900

    22-02-08) 세션 만료 시간 변경

---
+++ src/main/java/kong/tues/commons/session/SessionListener.java
@@ -0,0 +1,18 @@
+package kong.tues.commons.session;
+
+import javax.servlet.http.HttpSessionEvent;
+import javax.servlet.http.HttpSessionListener;
+
+public class SessionListener implements HttpSessionListener {
+
+    // 세션 만료 12시간
+    private final int sessionTime = 4320;
+
+    public void sessionCreate(HttpSessionEvent sessionEvent) {
+        sessionEvent.getSession().setMaxInactiveInterval(sessionTime);
+    }
+
+    public void sessionDestroyed(HttpSessionEvent sessionEvent) {
+
+    }
+}
