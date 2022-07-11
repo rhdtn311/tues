205d4452d441a8734023e0d13c3a93196db0d08f
kong <rhdtn311@naver.com>
2022-07-12 00:02:57 +0900

    feat : App, Main, Login, Home 뷰 구현 (+ index.ts)
    기준이 되는 뷰들의 구조 생성

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,5 +1,8 @@
 import { createRouter, createWebHistory } from "vue-router";
-import HomeView from "../views/HomeView.vue";
+import Find from "../components/home/Find.vue";
+import Login from "../components/home/Login.vue";
+import Join from "../components/home/Join.vue";
+import Main from "../components/main/Main.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -7,17 +10,29 @@ const router = createRouter({
     {
       path: "/",
       name: "home",
-      component: HomeView,
+      redirect:"/main",
     },
     {
-      path: "/about",
-      name: "about",
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import("../views/AboutView.vue"),
+      path: "/login",
+      name: "login",
+      component: Login
+    },
+    {
+      path: "/find",
+      name: "Find",
+      component: Find
+    },
+    {
+      path:"/join",
+      name:"Join",
+      component: Join
+    },
+    {
+      path: "/main",
+      name: "Main",
+      component: Main
     }
-  ],
+  ]
 });
 
 export default router;
