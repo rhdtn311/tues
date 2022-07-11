e2c998ca52e3600ae165cb3df7f293c5f2300818
kong <rhdtn311@naver.com>
2022-07-12 02:40:27 +0900

    feat : axios 설정 추가
    쿠키 전송을 위한 withCredential를 전역으로 설정,
    서버의 절대 경로를 전역 변수로 지정

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,12 +1,22 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
+import VueCookies from "vue-cookies";
 
 import App from "./App.vue";
 import router from "./router";
+import axios from "axios";
+
+// 쿠키 값 전송을 위한 withCredential를 true로 설정
+axios.defaults.withCredentials = true;
 
 const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
+app.use(VueCookies);
 
 app.mount("#app");
+
+
+// global
+app.config.globalProperties.server = "http://localhost:8080"
