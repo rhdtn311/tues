1048e783ac8287262525fcd64c6014874b7e77b4
kong <rhdtn311@naver.com>
2022-07-22 01:40:12 +0900

    chore : cors origin uri 변경

--- backend/src/main/java/kong/tues/commons/config/WebConfig.java
+++ backend/src/main/java/kong/tues/commons/config/WebConfig.java
@@ -44,7 +44,7 @@ public class WebConfig implements WebMvcConfigurer {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000")
+                        .allowedOrigins("http://tues.today")
                         .allowedHeaders("*")
                         .allowedMethods("*")
                         .exposedHeaders("*")
